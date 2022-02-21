3d9b9556c369fcb8dd98c899ba5438d037de9b04
Nico De Gols <nico@glue.be>
2022-02-21 19:37:51 +0100

    missing setMostviewedProducts function

---
+++ Plugin/ExtendProductList.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Glue\HyvaAutorelated\Plugin;
+
+class ExtendProductList Extends \Hyva\Theme\ViewModel\ProductList
+{
+
+    function setMostviewedProducts(array $items){
+
+    }
+    function setGroupId(){
+
+    }
+}
